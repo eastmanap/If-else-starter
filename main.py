954e69bfd82f96e9fe266434b3fe6bf1c0f21184
eastmanap <eastmanap354@northwested.org>
2024-10-04 11:14:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+# Apollos Eastman
+# 10/4/2024
+# If Else Starter
+
+# Task 2
+
+# Task 5-3
+
+alien_color = input ('Enter the color of a alien:')
+
+if alien_color is 'green':
+    print ('You get 5 points!')
+
+alien_color = 'red'
+
+if alien_color is 'green':
+    print ('You get 5 points!')
+
+# Task 5-4
+print()
+    
+alien_color1 = input('Enter the color of a alien:')
+
+if alien_color1 is 'green':
+    print ('You get 5 points!')
+else:
+    print ('You get 10 points!')
+
+# Task 3
+print()
+
+first_name = input('Enter your first name:')
+
+if len(first_name) <= 5:
+    print('Your name is too short.')
+else:
+    print('Your name is too long.')
+
+# Task 4
+print()
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+letter = input('Please enter a letter of the alphabet:\n').lower
+
+if letter in vowels:
+    print('Your letter is a vowel.')
+else:
+    print('Your letter is not a vowel.')
+
+# Task 5
+print()
+
+num = int(input('Enter a integer:'))
+num1 = int(input('Enter a second integer:'))
+
+if (num % num1) is 0:
+    print('The the first number is divisible by the second number.')
+else:
+     print('The the first number is not divisible by the second number.')
+
+
